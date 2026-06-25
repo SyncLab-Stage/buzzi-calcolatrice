1305f3f9852783d6727b95ecc2cf672dea3e8a83
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 11:31:50 +0200

    feat: add Scanner class to take input; no hardcoded values

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Calcolatrice calcolatrice = new Calcolatrice();
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Valore A: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Valore B: ");
+        double b = scanner.nextDouble();
+
         try {
-            calcolatrice.quoziente(2, 0)
+            System.out.println(calcolatrice.quoziente(a, b));
         } catch (DivisionePerZeroException e) {
             System.out.println(e.getMessage());
         }
+
+        System.out.println();
     }
 }
