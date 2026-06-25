7047ac9014d9e7991cbf495ab6857de8642824f7
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 12:19:17 +0200

    fix: remove loop, fix behavior

--- src/Main.java
+++ src/Main.java
@@ -10,38 +10,37 @@ public class Main {
         int opzione = 0;
         double a = 0;
         double b = 0;
-        do {
-            // menu
-            System.out.println("Selezionare l'opzione del calcolo da eseguire");
+        // menu
+        System.out.println("Selezionare l'opzione del calcolo da eseguire");
 
-            System.out.printf("%-20s %d%n", "Addizione:", 1);
-            System.out.printf("%-20s %d%n", "Sottrazione:", 2);
-            System.out.printf("%-20s %d%n", "Moltiplicazione:", 3);
-            System.out.printf("%-20s %d%n", "Divisione:", 4);
-            System.out.printf("%-20s %d%n", "Uscita:", 0);
-            System.out.printf("Inserire opzione: ");
-            opzione = scanner.nextInt();
-            if (opzione != 0) {
-                // user input
-                System.out.print("Valore a: ");
-                a = scanner.nextDouble();
+        System.out.printf("%-20s %d%n", "Addizione:", 1);
+        System.out.printf("%-20s %d%n", "Sottrazione:", 2);
+        System.out.printf("%-20s %d%n", "Moltiplicazione:", 3);
+        System.out.printf("%-20s %d%n", "Divisione:", 4);
+        System.out.printf("%-20s %d%n", "Uscita:", 0);
+        System.out.printf("Inserire opzione: ");
+        opzione = scanner.nextInt();
+        if (opzione != 0) {
+            // user input
+            System.out.print("Valore a: ");
+            a = scanner.nextDouble();
 
-                System.out.print("Valore b: ");
-                b = scanner.nextDouble();
-            }
-            switch (opzione) {
-                case 1 -> System.out.println(calcolatrice.somma(a, b));
-                case 2 -> System.out.println(calcolatrice.differenza(a, b));
-                case 3 -> System.out.println(calcolatrice.prodotto(a, b));
-                case 4 -> {
-                    try {
-                        System.out.println(calcolatrice.quoziente(a, b));
-                    } catch (DivisionePerZeroException e) {
-                        System.out.println(e.getMessage());
-                    }
+            System.out.print("Valore b: ");
+            b = scanner.nextDouble();
+        }
+        switch (opzione) {
+            case 1 -> System.out.println(calcolatrice.somma(a, b));
+            case 2 -> System.out.println(calcolatrice.differenza(a, b));
+            case 3 -> System.out.println(calcolatrice.prodotto(a, b));
+            case 4 -> {
+                try {
+                    System.out.println(calcolatrice.quoziente(a, b));
+                } catch (DivisionePerZeroException e) {
+                    System.out.println(e.getMessage());
                 }
-                default -> System.out.println("Opzione non valida.");
-            };
-        } while (opzione != 0);
+            }
+            default -> System.out.println("Opzione non valida.");
+        }
+        ;
     }
 }
