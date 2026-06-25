7ec0a7b408cb207b37ee992dbc2c690ea56d7787
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 11:23:09 +0200

    feat: add calcolatrice.java class v1

---
+++ src/Calcolatrice.java
@@ -0,0 +1,15 @@
+public class Calcolatrice {
+    public double somma(double a, double b) {
+        return a + b;
+    }
+
+    public double differenza(double a, double b) {
+        return a - b;
+    }
+    public double prodotto(double a, double b) {
+        return a * b;
+    }
+    public double quoziente(double a, double b) {
+        return a / b;
+    }
+}
