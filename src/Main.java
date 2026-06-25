d9ed69bf99f0664f270b6005fd78f4c01c48a8bd
corinnabuzzi <corinnabuzzi48@gmail.com>
2026-06-25 11:27:46 +0200

    feat: try/catch in Main, v1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Calcolatrice calcolatrice = new Calcolatrice();
+
+        try {
+            calcolatrice.quoziente(2, 0)
+        } catch (DivisionePerZeroException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
